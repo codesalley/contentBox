b10227abaa522d7d45458714c6b51114489b11bb
Code Salley <salley@ferch.ly>
2022-02-16 12:44:42 +0000

    grnad import

---
+++ index.js
@@ -0,0 +1,9 @@
+import 'assets/minimalist-blocks/content.css';
+import 'box/box-flex.css';
+import 'contentbuilder/contentbuilder.css';
+import 'contentbox/contentbox.css';
+import 'assets/scripts/glide/css/glide.core.css';
+import 'assets/scripts/glide/css/glide.theme.css';
+import 'assets/scripts/glide/glide.js';
+import 'contentbox/lang/en.js';
+import 'contentbuilder/contentbuilder.min.js';
